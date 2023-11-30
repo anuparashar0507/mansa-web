99d986d445874b7f5a7ca6fe6502cfddbfcac7f0
anuparashar0507 <anuparashar0507@gmail.com>
2023-12-01 04:05:27 +0530

    Initial setup

---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Contact Us - MANSA</title>
+        <meta name="description" content="Contact Us - MANSA" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      
+      </main>
+    </>
+  );
+}
---
+++ src/pages/gallery.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Gallery() {
+  return (
+    <>
+      <Head>
+        <title>Gallery - MANSA</title>
+        <meta name="description" content="Gallery - MANSA" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      
+      </main>
+    </>
+  );
+}
